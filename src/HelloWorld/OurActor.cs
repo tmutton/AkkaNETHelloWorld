e9ffb49c437e504c5f919055fe1964f2efe0a52e
Thomas Mutton <tmutton@businesssystemsuk.com>
2015-07-06 11:33:09 +0100

    regressed to a more simpler hello world app

---
+++ src/HelloWorld/OurActor.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld
+{
+    using System;
+
+    using Akka.Actor;
+
+    public class OurActor : ReceiveActor
+    {
+        public OurActor()
+        {
+            // when we tell this actor something it will write our message to the console
+            this.Receive<string>(x => Console.WriteLine(x));
+        }
+    }
+}
---
+++ src/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HelloWorld
+{
+    using System;
+
+    using Akka.Actor;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a new actor system (a container for actors)
+            var system = ActorSystem.Create("MySystem");
+
+            // create actor and get a reference to it.
+            var actor = system.ActorOf<OurActor>();
+
+            // create a message to send to the actor
+            const string MessageToSendToActor = "Hello World!";
+
+            // send our message to the actor
+            actor.Tell(MessageToSendToActor);
+
+            Console.ReadLine(); // keep console alive
+        }
+    }
+}
